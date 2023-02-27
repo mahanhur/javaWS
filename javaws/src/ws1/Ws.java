93a6f9807376e974644700d1e84b633e7142a780
mahanhur <allowbasmh@gmail.com>
2023-02-27 17:13:42 +0900

    mahanhur a1

--- javaws/src/ws1/Ws.java
+++ javaws/src/ws1/Ws.java
@@ -1,5 +1,6 @@
 package ws1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ws {
@@ -20,8 +21,29 @@ public class Ws {
 				// 배열 정보를 출력한다.
 				// 배열의 합과 평균을 출력 한다.
 				
+				int sumA1 = 0;
+				double avgA1 = 0.0;
+				
+				
 				System.out.println("Input Number[5~9]");
-				String Numa1 = "";
+				// 입력받은 숫자를 int로 변환하여 그 갯수만큼 배열 생성
+				int arrA1[] = new int[Integer.parseInt(sc.next())];
+							
+				
+				//랜덤한 숫자 생성하여 배열에 넣는다
+				Random r = new Random();
+				for(int i : arrA1) {
+					arrA1[i] = r.nextInt(9)+1;
+					sumA1 += arrA1[i];
+					//배열 정보 출력
+					System.out.printf("%d \t",arrA1[i]);
+				}
+				//평균 계산
+				avgA1 = sumA1/ (arrA1.length * 1.0);
+				
+				//합과 평균 출력
+				System.out.printf("\n합: %d, 평균: %4.2f\n", sumA1, avgA1);
+						
 				
 			} else if(cmd.equals("a2")) {
 				// 5~9까지의 숫자를 입력 받는다.
@@ -43,7 +65,7 @@ public class Ws {
 				// 1~9까지의 랜덤한 숫자를 배열에 넣는다.
 				// 배열 정보를 출력한다.
 				// 배열의 합과 평균을 출력 한다.
-				
+
 			} else {
 				System.out.println("Invalid Command");
 				System.out.println("Try Again");
