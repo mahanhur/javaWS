d6fa707081d67822e1f055ebdc1ea1e5206825f4
jiyoung0123 <taebin100@gmail.com>
2023-02-28 17:49:36 +0900

    employee0228

--- javaws/src/ws0228/App.java
+++ javaws/src/ws0228/App.java
@@ -3,8 +3,25 @@ package ws0228;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Employee empArr[] = new Employee[5];
+		
+		empArr[0] = new Employee("0001","David",1000);
+		empArr[1] = new Employee("0002","Tom",2000);
+		empArr[2] = new Employee("0003","Jenny",3000);
+		empArr[3] = new Employee("0004","Rose",4000);
+		empArr[4] = new Employee("0005","Clara",5000);
+		
+		
+		for(Employee k : empArr){
+			System.out.println(k);
+			System.out.printf("연봉은 %d, 세금은 %4.2f \n", k.getAnnSalary() , k.getTax());
+			System.out.println("---------");
+		}
+		
+	
+	}
+		
 
 	}
 
-}
+
